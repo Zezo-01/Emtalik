0d3cbe28e99b00e7891a05e44f1cc1a3e7cef793
Zezo-0 <j.demogharbe@gmail.com>
2022-04-03 02:12:36 +0300

    commit

---
+++ lib/Widgets/appenv.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+abstract class AppEnv {
+  // This is not good colors and needs to be changed
+
+  // LIGHT THEME COLOR SCHEME
+  static const ColorScheme _defaultLightColorScheme = ColorScheme(
+    brightness: Brightness.light,
+    primary: Colors.blueAccent,
+    onPrimary: Colors.black26,
+    background: Colors.blueGrey,
+    onBackground: Colors.indigo,
+    error: Color.fromARGB(255, 124, 8, 0),
+    onError: Colors.redAccent,
+    surface: Colors.white,
+    onSurface: Colors.grey,
+    secondary: Colors.green,
+    onSecondary: Color.fromARGB(255, 1, 109, 4),
+  );
+
+  // DARK THEME COLOR SCHEME
+  static const ColorScheme _defaultDarkColorScheme = ColorScheme(
+    brightness: Brightness.light,
+    primary: Colors.blueAccent,
+    onPrimary: Colors.black26,
+    background: Colors.blueGrey,
+    onBackground: Colors.indigo,
+    error: Color.fromARGB(255, 124, 8, 0),
+    onError: Colors.redAccent,
+    surface: Colors.white,
+    onSurface: Colors.grey,
+    secondary: Colors.green,
+    onSecondary: Color.fromARGB(255, 1, 109, 4),
+  );
+
+  // LIGHT THEME DATA
+  static get defaultLightTheme => ThemeData(
+        colorScheme: _defaultLightColorScheme,
+        scaffoldBackgroundColor: const Color.fromARGB(255, 127, 93, 182),
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Color.fromARGB(255, 236, 166, 61),
+          foregroundColor: Colors.black,
+        ),
+      );
+  // DARK THEME DATA
+  static get defaultDarkTheme => ThemeData(
+        colorScheme: _defaultDarkColorScheme,
+        scaffoldBackgroundColor: const Color.fromARGB(255, 127, 93, 182),
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Color.fromARGB(255, 236, 166, 61),
+          foregroundColor: Colors.black,
+        ),
+      );
+}
