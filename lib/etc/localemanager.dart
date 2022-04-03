d7cd2d4a3967da657dff5300af72373f429ce8d3
Zezo-0 <j.demogharbe@gmail.com>
2022-04-04 02:47:59 +0300

    added functions to switch language check lib/etc/localemanager.dart

--- lib/etc/localemanager.dart
+++ lib/etc/localemanager.dart
@@ -3,6 +3,7 @@ import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:localization/localization.dart';
+import 'package:localization/src/localization_service.dart';
 
 abstract class LocaleManager {
   static const List<Locale> supportedLocales = [
@@ -16,4 +17,14 @@ abstract class LocaleManager {
     GlobalCupertinoLocalizations.delegate,
     LocalJsonLocalization.delegate
   ];
+
+  static void selectEnglish() {
+    LocalizationService localeService = LocalizationService.instance;
+    localeService.changeLanguage(const Locale('en'), ['lib/i18n']);
+  }
+
+  static void selectArabic() {
+    LocalizationService localeService = LocalizationService.instance;
+    localeService.changeLanguage(const Locale('ar'), ['lib/i18n']);
+  }
 }
