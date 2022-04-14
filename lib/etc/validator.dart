29f05f7344dbe3d03372ea0dcf8dc8bc49d2378b
Zezo-0 <j.demogharbe@gmail.com>
2022-04-15 02:29:47 +0300

    shit

--- lib/etc/validator.dart
+++ lib/etc/validator.dart
@@ -1,7 +1,7 @@
 abstract class Validator {
   static bool passwordValidator(String password) {
     if (RegExp(
-      r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$',
+      r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,25}$',
     ).hasMatch(password)) return true;
     return false;
   }
