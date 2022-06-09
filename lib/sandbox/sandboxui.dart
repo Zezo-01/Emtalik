983d59b933ffd13f76810a769b61ddecbdd4940a
Fadi Shalash <fadielshalash@gmail.com>
2022-06-10 00:24:41 +0300

    Grid at SandBoxui

--- lib/sandbox/sandboxui.dart
+++ lib/sandbox/sandboxui.dart
@@ -11,21 +11,19 @@ class SandBoxUi extends StatelessWidget {
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
-      child: Scaffold(
-        body: SingleChildScrollView(
-          child: DisplayCard(
-            onPress: () {
-              debugPrint("HELLO");
-            },
-            header: "Header",
-            footer1: "Footer1",
-            footer2: "Footer2",
-            imageNetworkPath:
-                "https://images.unsplash.com/photo-1453728013993-6d66e9c9123a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dmlld3xlbnwwfHwwfHw%3D&w=1000&q=80",
-            borderColor: Colors.amber,
-          ),
-        ),
+      child: GridView.count(
+        crossAxisCount: 2,
+        mainAxisSpacing: 10,
+        childAspectRatio: 2/4,
+        children: List.generate(10, (index){
+         return Card(
+           shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+           child: DisplayCard(borderColor: Colors.blue,footer1: 'Abo Sator',footer2: 'ابو ساطور',header: 'Bab Al7ara',imageNetworkPath: 'https://al-arrab.com/wp-content/uploads/2022/01/Doc-P-907467-637778562956208455.jpg'),
+         );
+        },
       ),
+      
+    )
     );
   }
 }
